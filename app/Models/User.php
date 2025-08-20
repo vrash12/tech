e8563403ad0a76bf4ba4a98c367a028c68b34160
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-08-21 00:45:49 +0800

    fix register

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,8 +11,12 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password', 'role',
+        'username',
+        'name',
+        'email',
+        'password',
     ];
+
 
     protected $hidden = [
         'password', 'remember_token',
--- backend/app.py
+++ backend/app.py
@@ -51,6 +51,6 @@ def retrain():
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
+        port=int(os.environ.get("PORT", 5001)),
         debug=True
     )
